44aa3c79056331339c57115c7cb9f0942fef6aca
PedroLucas0409 <plmaia43@gmail.com>
2024-08-29 22:12:56 -0300

    Atualizando Rotas

--- App.tsx
+++ App.tsx
@@ -1,27 +1,28 @@
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import LoginScreen from './screens/LoginScreen';
 import { createStackNavigator } from '@react-navigation/stack';
-import MenuScreen from './screens/MenuScreen';
-import EstoqueScreen from './screens/EstoqueScreen';
-import ProducaoScreen from './screens/ProducaoScreen';
-import SenhaResetScreen from './screens/SenhaResetScreen';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import LoginScreen from "./screens/LoginScreen";
+import MenuScreen from "./screens/MenuScreen";
+import EstoqueScreen from "./screens/EstoqueScreen";
+import ProducaoScreen from "./screens/ProducaoScreen";
+import SenhaResetScreen from "./screens/SenhaResetScreen";
+import MontagemProd from "./screens/MontagemProd";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Login'>
+      <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Menu" component={MenuScreen} />
         <Stack.Screen name="Estoque" component={EstoqueScreen} />
         <Stack.Screen name="Producao" component={ProducaoScreen} />
         <Stack.Screen name="SenhaReset" component={SenhaResetScreen} />
+        <Stack.Screen name="MontagemProd" component={MontagemProd} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-
