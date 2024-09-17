1c3977a3b0527a4c1eb8032f588f756cceafcc98
PedroLucas0409 <plmaia43@gmail.com>
2024-09-16 21:33:53 -0300

    Atualizando definições de variáveis!

--- client/src/types.ts
+++ client/src/types.ts
@@ -1,29 +1,20 @@
 export type RootStackParamList = {
   Login: undefined;
   Menu: undefined;
-  AutoclaveProd: undefined;
-  MontagemProd: undefined;
   SenhaReset: undefined;
   Buttons: undefined;
   ExamScreen: undefined;
+  RaspaScreen: undefined; 
   EscareacaoScreen: undefined;
-  ExameFinalProd: undefined;
   ProducaoScreen:undefined;
   ConfirmationExam: { status: string; orderNumber: string; tireId: string };
-<<<<<<< HEAD:src/types.ts
-  RaspaScreen: undefined;
-  ConfirmationRaspaScreen: { status: string; width: string; perimeter: string };
   AplicarColaScreen: undefined;
   Orbicushion: undefined;
   AutoclaveProd: undefined;
   ConfirmationAutoclave: { selectedValue: string; position: string; load: string; }; 
   MontagemProd: undefined;
-  ConfirmationMontagem: { status: string; orderNumber: string; assemblyId: string };
-=======
   ConfirmationEscareacaoScreen: { status: string; orderNumber: string; tireId: string };
-  RaspaScreen: undefined; 
   ConfirmationRaspaScreen: { status: string; width: string; perimeter: string }; 
->>>>>>> main:client/src/types.ts
   AplicarBandaScreen: { status: string; width: string; perimeter: string };
   ExameFinalProd: undefined;
   ConfirmationExFinal: { status: string; date: string };
