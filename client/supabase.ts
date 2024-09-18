0394911c0b29ea7248d57d49bebf2918eae98da6
Gabriel Feitosa <gabrielfcb13@gmail.com>
2024-09-18 11:01:00 -0300

    Criado arquivo separado para chaves do projeto

--- client/supabase.ts
+++ client/supabase.ts
@@ -2,9 +2,7 @@ import { AppState } from "react-native";
 import "react-native-url-polyfill/auto";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "REACT_APP_SUPABASE_URL";
-const supabaseAnonKey = "REACT_APP_SUPABASE_ANON_KEY";
+import { supabaseUrl, supabaseAnonKey } from "./env";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
