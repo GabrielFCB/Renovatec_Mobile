40374879f7d4265c6232d67efd68ed5583f4dbda
Gabriel Feitosa <gabrielfcb13@gmail.com>
2024-10-02 11:47:03 -0300

    Criado controller de Coleta  para testes com Web. Corrigido bug de rota no controle de pneu

--- server/controller/pneuController.js
+++ server/controller/pneuController.js
@@ -1,42 +1,91 @@
+// controllers/pneuController.js
 const supabase = require("../supabaseClient");
 
-exports.login = async (req, res) => {
+exports.insertPneu = async (req, res) => {
+  const {
+    codigo_pneu,
+    matricula,
+    tamanho,
+    marca,
+    modelo,
+    DOT,
+    servico,
+    valor,
+    ID_Coleta,
+  } = req.body;
   try {
-    const { email, password } = req.body;
-    const response = await supabase.auth.signInWithPassword({
-      email,
-      password,
+    const response = await supabase.from("Pneu").insert({
+      codigo_pneu,
+      matricula,
+      tamanho,
+      marca,
+      modelo,
+      DOT,
+      servico,
+      valor,
+      status: "Recebido",
+      ID_Coleta,
     });
+
+    if (response.error) {
+      console.error("Error in insertPneu:", response.error);
+      throw response.error;
+    }
+
+    res.status(201).json(response);
+  } catch (error) {
+    console.error("Caught error in insertPneu:", error.message);
+    res.status(400).json({ error: error.message });
+  }
+};
+
+exports.getPneuByColeta = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const response = await supabase.from("Pneu").select().eq("ID_Coleta", id);
     const { data, error } = response;
 
-    if (error) {
-      throw error;
-    }
+    if (error) throw error;
+    res.status(201).json(response);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
+exports.updatePneu = async (req, res) => {
+  const { id } = req.params;
+  const {
+    codigo_pneu,
+    matricula,
+    tamanho,
+    marca,
+    modelo,
+    DOT,
+    servico,
+    valor,
+    ID_Coleta,
+  } = req.body;
+  try {
+    const response = await supabase
+      .from("Pneu")
+      .update({
+        codigo_pneu,
+        matricula,
+        tamanho,
+        marca,
+        modelo,
+        DOT,
+        servico,
+        valor,
+        status: "Recebido",
+        ID_Coleta,
+      })
+      .eq("ID_Pneu", id);
+    const { data, error } = response;
+
+    if (error) throw error;
     res.status(200).json(response);
   } catch (error) {
-    console.error("Erro ao fazer login:", error.message);
-    res.status(500).json({ error: "Internal server error" });
+    res.status(400).json({ error: error.message });
   }
-};
-
-exports.signOut = async (req, res) => {
-  try {
-    const { error } = await supabase.auth.signOut();
-    if (error) {
-      throw error;
-    }
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-};
-
-exports.passwordReset = async (req, res) => {
-  const { email } = req.body;
-  const { error } = await supabase.auth.resetPasswordForEmail(email, {
-    redirectTo: "http://localhost:3000/senha",
-  });
-  if (error) return res.status(400).json({ error: error.message });
-  res.status(200).json({ message: "Password reset email sent" });
 };
