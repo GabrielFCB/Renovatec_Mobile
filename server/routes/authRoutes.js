5cf25f444d7d2ec5ef8c287b75fd1154c3e83640
rodzg <118945975+rodzg@users.noreply.github.com>
2024-10-16 15:32:50 -0300

    Revert "tela de corte banda e routes"

--- server/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authController = require("../controllers/authController");
-
-router.post("/login", authController.login);
-router.post("/signOut", authController.signOut);
-router.post("/passwordReset", authController.passwordReset);
-
-module.exports = router;
--- server/routes/pneuColetaRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/coletaRoutes.js
-const express = require("express");
-const { insertColetaPneu } = require("../controllers/pneuColetaController");
-
-const router = express.Router();
-
-router.post("/coletas-pneus", insertColetaPneu);
-
-module.exports = router;
