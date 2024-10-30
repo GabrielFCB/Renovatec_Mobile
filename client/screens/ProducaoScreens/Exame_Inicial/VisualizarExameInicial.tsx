a41663f97d7748a72f4c7cbbb9ee0808dda9619d
PedroLucas0409 <plmaia43@gmail.com>
2024-10-30 15:43:10 -0300

    Corrigindo bug RootStackParamList!

--- client/screens/ProducaoScreens/Exame_Inicial/VisualizarExameInicial.tsx
+++ client/screens/ProducaoScreens/Exame_Inicial/VisualizarExameInicial.tsx
@@ -27,7 +27,7 @@ const VisualizarExameInicial: React.FC<Props> = ({ navigation }) => {
     const fetchData = async () => {
       const { data, error } = await supabase
         .from("Pneu")
-        .select("ID_Pneu, codigo_pneu, status, ID_Coleta")
+        .select("ID_Pneu, codigo_pneu, status, ID_Coleta, perimeter, width")
         .eq("Etapa_Producao", "ExameInicial");
 
       if (error) {
